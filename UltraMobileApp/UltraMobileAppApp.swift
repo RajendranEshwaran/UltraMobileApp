4b4d702ce6e3ebb3526f9b5e5d88eee28d0d4ac0
RAJENDRAN ESHWARAN <10699484+RajendranEshwaran@users.noreply.github.com>
2025-08-14 15:58:02 -0400

    Initial Commit

---
+++ UltraMobileApp/UltraMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltraMobileAppApp.swift
+//  UltraMobileApp
+//
+//  Created by RajayGoms on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct UltraMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
